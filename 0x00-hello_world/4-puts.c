426a53d39ca6e27546fd7b1576e5a2e8e116f138
AgutuSam <sammyhardcell@gmail.com>
2022-09-08 12:25:15 -0700

    it is a  cold world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -10,7 +10,7 @@ int main(void)
 {
 	char myChar[55];
 
-	strcpy(myChar, "Programming is like building a multilingual puzzle");
+	strcpy(myChar, "\"Programming is like building a multilingual puzzle");
 
 	puts(myChar);
 
