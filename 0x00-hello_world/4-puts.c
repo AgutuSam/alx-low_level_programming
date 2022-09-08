7e9ad1ffeb11ee7f899f9d03bb1456464e6471be
AgutuSam <sammyhardcell@gmail.com>
2022-09-08 12:21:52 -0700

    puts see

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char myChar[55];
+
+	strcpy(myChar, "Programming is like building a multilingual puzzle\n");
+
+	puts(myChar);
+
+	return (0);
+}
