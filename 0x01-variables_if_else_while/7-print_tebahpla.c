07b21c4195deeb664639f7b9a05761bd80cf5cfd
AgutuSam <sammyhardcell@gmail.com>
2022-09-09 06:13:07 -0700

    reverse alpha

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *  *  * main - Entry point
+ *   *   *
+ *    *    * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char n;
+
+/* your code goes there */
+	for (n = 'z'; n >= 'a'; n--)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+/* your code goes there */
+	return (0);
+}
