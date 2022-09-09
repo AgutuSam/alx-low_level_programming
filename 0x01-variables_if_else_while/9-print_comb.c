652e8314f30c0b7934c08d664d5e809334e101d3
AgutuSam <sammyhardcell@gmail.com>
2022-09-09 11:57:08 -0700

    betty is now fine with my code

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,14 +2,17 @@
 #include<string.h>
 
 /**
- *  *  * print - Entry point
- *   *   * num - an integer
- *    *    * n - as an integer
+ *  *  * main - Entry point
+ *   *   *
+ *    *    * Return: Always 0 (Success)
  */
-/* num as a parameter for func print*/
-/* n as a parameter for func print*/
-void print(int *num, int n) /* num and n are integers*/
+
+int main(void)
 {
+	int num[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+	int n = 10;
+
+/* your code goes there */
 	int i;
 
 	for (i = 0 ; i < n ; i++)
@@ -23,32 +26,6 @@ void print(int *num, int n) /* num and n are integers*/
 			putchar((num[i] % 10) + '0');
 			putchar('\n');
 		}
-}
-
-/**
- *  *  * main - Entry point
- *   *   *
- *    *    * Return: Always 0 (Success)
- */
-
-int main(void)
-{
-	int num[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-	int temp;
-	int i;
-	int j;
-	int n = 10;
-/* your code goes there */
-	for (j = 1; j <= n; j++)
-	{
-		for (i = 0; i < n - 1; i++)
-		{
-			temp = num[i];
-			num[i] = num[i + 1];
-			num[i + 1] = temp;
-			print(num, n);
-		}
-	}
 /* your code goes there */
 	return (0);
 }
