86560da00480e3fbdc3981f4ea8dadaf7a15829f
AgutuSam <sammyhardcell@gmail.com>
2022-09-13 14:17:24 -0700

    alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *  *  * print_alphabet - Entry point
+ *   *   *
+ *    *    * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char n;
+
+/* your code goes there */
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+/* your code goes there */
+}
