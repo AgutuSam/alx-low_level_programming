b8bb3a39551688a1eb3db9854682faef9a989b21
AgutuSam <sammyhardcell@gmail.com>
2022-09-14 07:29:39 -0700

    alphabet 10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ *  *  * print_alphabet - Entry point
+ *   *   *
+ *    *    * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	char n;
+	int count;
+
+/* your code goes there */
+	count = 0;
+	while (count < 10)
+	{
+		n = 'a';
+		while (n <= 'z')
+		{
+			_putchar(n);
+			n++;
+		}
+		_putchar('\n');
+		count++;
+	}
+/* your code goes there */
+}
