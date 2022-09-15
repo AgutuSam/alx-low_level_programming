662f3d6a398737a5eb2fbd6c44abdf10508f85f8
AgutuSam <sammyhardcell@gmail.com>
2022-09-15 14:51:35 -0700

    no space for 100

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,11 +12,13 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if ((i % 15) == 0 && i != 0)
+		if (i == 100)
+			printf("Buzz");
+		else if ((i % 15) == 0 )
 			printf("FizzBuzz ");
-		else if ((i % 5) == 0 && i != 0)
+		else if ((i % 5) == 0)
 			printf("Buzz ");
-		else if ((i % 3) == 0 && i != 0)
+		else if ((i % 3) == 0)
 			printf("Fizz ");
 		else
 			printf("%d ", i);
